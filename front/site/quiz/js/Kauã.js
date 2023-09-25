3d0e9cc36cfe16962c322b9f8de230091fd902ed
kauabarros-24 <martinsbarroskaua@gmail.com>
2023-09-25 14:39:09 -0300

    Fiz até a pergunta 5

--- front/site/quiz/js/Kauã.js
+++ front/site/quiz/js/Kauã.js
@@ -3,6 +3,7 @@ const pg1 = document.getElementById("secao-pergunta-1");
 const pg2 = document.getElementById("secao-pergunta-2");
 const pg3 = document.getElementById("secao-pergunta-3");
 const pg4 = document.getElementById("secao-pergunta-4");
+const pg5 = document.getElementById("secao-pergunta-5");
 const resCerta = document.getElementById("res-certa");
 const resErrada = document.getElementById("res-errada");
 
@@ -94,7 +95,43 @@ function responder3() {
                     pg4.style.display = 'grid';
                 }, 2000);
             }
+            else {
+                pg3.style.display = "none";
+                resErrada.style.display = "grid";
+                setTimeout(() =>{
+                    resErrada.style.display = "none";
+                    pg4.style.display = "grid";
+                }, 2000);
+            }
         }
     }
 }
 document.getElementById("btnPergunta3").addEventListener("click", responder3);
+
+function responder4() {
+    let resposta = document.getElementsByName("questao4");
+    let valorResposta = ""; 
+    
+    for (let o = 0; o < resposta.length; o++) {
+        if (resposta[o].checked) {
+            valorResposta = resposta[o].value;
+            if (valorResposta == 'certo') {
+                pg4.style.display = 'none';
+                resCerta.style.display = 'grid';
+                setTimeout(() => {
+                    resCerta.style.display = 'none';
+                    pg5.style.display = 'grid';
+                }, 2000);
+            }
+            else {
+                pg4.style.display = "none";
+                resErrada.style.display = "grid";
+                setTimeout(() =>{
+                    resErrada.style.display = "none";
+                    pg5.style.display = "grid";
+                }, 2000);
+            }
+        }
+    }
+}
+document.getElementById("btnPergunta4").addEventListener("click", responder4);
