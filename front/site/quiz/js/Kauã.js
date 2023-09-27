a327a68c0c08e9f50373291aed1757cc3611e9d1
kauabarros-24 <martinsbarroskaua@gmail.com>
2023-09-27 13:34:38 -0300

    As questões 9 e 10 dos mitos nórdicos estão feitas

--- front/site/quiz/js/Kauã.js
+++ front/site/quiz/js/Kauã.js
@@ -6,6 +6,8 @@ const pg5 = document.getElementById("secao-pergunta-5");
 const pg6 = document.getElementById("secao-pergunta-6");
 const pg7 = document.getElementById("secao-pergunta-7");
 const pg8 = document.getElementById("secao-pergunta-8");
+const pg9 = document.getElementById("secao-pergunta-9");
+const pg10 = document.getElementById("secao-pergunta-10");
 const resCerta = document.getElementById("res-certa");
 const resErrada = document.getElementById("res-errada");
 
@@ -38,7 +40,7 @@ function responder1() {
                     resCerta.style.display = "none";
                     pg2.style.display = "grid";
                 }, 2000);
-            } 
+            }
             else if (valorResposta === "errado") {
                 pg1.style.display = "none";
                 resErrada.style.display = "grid";
@@ -47,7 +49,7 @@ function responder1() {
                     pg2.style.display = "grid";
                 }, 2000);
             }
-    
+
         }
     }
 }
@@ -84,8 +86,8 @@ document.getElementById("btnPergunta2").addEventListener("click", responder2);
 
 function responder3() {
     let resposta = document.getElementsByName("questao3");
-    let valorResposta = ""; 
-    
+    let valorResposta = "";
+
     for (let d = 0; d < resposta.length; d++) {
         if (resposta[d].checked) {
             valorResposta = resposta[d].value;
@@ -100,7 +102,7 @@ function responder3() {
             else {
                 pg3.style.display = "none";
                 resErrada.style.display = "grid";
-                setTimeout(() =>{
+                setTimeout(() => {
                     resErrada.style.display = "none";
                     pg4.style.display = "grid";
                 }, 2000);
@@ -112,8 +114,8 @@ document.getElementById("btnPergunta3").addEventListener("click", responder3);
 
 function responder4() {
     let resposta = document.getElementsByName("questao4");
-    let valorResposta = ""; 
-    
+    let valorResposta = "";
+
     for (let o = 0; o < resposta.length; o++) {
         if (resposta[o].checked) {
             valorResposta = resposta[o].value;
@@ -128,7 +130,7 @@ function responder4() {
             else {
                 pg4.style.display = "none";
                 resErrada.style.display = "grid";
-                setTimeout(() =>{
+                setTimeout(() => {
                     resErrada.style.display = "none";
                     pg5.style.display = "grid";
                 }, 2000);
@@ -140,8 +142,8 @@ document.getElementById("btnPergunta4").addEventListener("click", responder4);
 
 function responder5() {
     let resposta = document.getElementsByName("questao5");
-    let valorResposta = ""; 
-    
+    let valorResposta = "";
+
     for (let o = 0; o < resposta.length; o++) {
         if (resposta[o].checked) {
             valorResposta = resposta[o].value;
@@ -156,7 +158,7 @@ function responder5() {
             else {
                 pg5.style.display = 'none';
                 resErrada.style.display = 'grid';
-                setTimeout(() =>{
+                setTimeout(() => {
                     resErrada.style.display = 'none';
                     pg6.style.display = 'grid';
                 }, 2000);
@@ -168,8 +170,8 @@ document.getElementById("btnPergunta5").addEventListener("click", responder5)
 
 function responder6() {
     let resposta = document.getElementsByName("questao6");
-    let valorResposta = ""; 
-    
+    let valorResposta = "";
+
     for (let contador6 = 0; contador6 < resposta.length; contador6++) {
         if (resposta[contador6].checked) {
             valorResposta = resposta[contador6].value;
@@ -184,7 +186,7 @@ function responder6() {
             else {
                 pg6.style.display = 'none';
                 resErrada.style.display = 'grid';
-                setTimeout(() =>{
+                setTimeout(() => {
                     resErrada.style.display = 'none';
                     pg7.style.display = 'grid';
                 }, 2000);
@@ -196,8 +198,8 @@ document.getElementById("btnPergunta6").addEventListener("click", responder6);
 
 function responder7() {
     let resposta = document.getElementsByName("questao7");
-    let valorResposta = ""; 
-    
+    let valorResposta = "";
+
     for (let contador7 = 0; contador7 < resposta.length; contador7++) {
         if (resposta[contador7].checked) {
             valorResposta = resposta[contador7].value;
@@ -212,7 +214,7 @@ function responder7() {
             else {
                 pg7.style.display = 'none';
                 resErrada.style.display = 'grid';
-                setTimeout(() =>{
+                setTimeout(() => {
                     resErrada.style.display = 'none';
                     pg8.style.display = 'grid';
                 }, 2000);
@@ -222,3 +224,61 @@ function responder7() {
 }
 
 document.getElementById("btnPergunta7").addEventListener("click", responder7);
+
+function responder8() {
+    let resposta = document.getElementsByName("questao8");
+    let valorResposta = "";
+
+    for (let contador8 = 0; contador8 < resposta.length; contador8++) {
+        if (resposta[contador8].checked) {
+            valorResposta = resposta[contador8].value;
+            if (valorResposta == 'certo') {
+                pg8.style.display = 'none';
+                resCerta.style.display = 'grid';
+                setTimeout(() => {
+                    resCerta.style.display = 'none';
+                    pg9.style.display = 'grid';
+                }, 2000);
+            }
+            else {
+                pg8.style.display = 'none';
+                resErrada.style.display = 'grid';
+                setTimeout(() => {
+                    resErrada.style.display = 'none';
+                    pg9.style.display = 'grid';
+                }, 2000);
+            }
+        }
+    }
+}
+document.getElementById("btnPergunta8").addEventListener("click", responder8)
+
+function responder9() {
+    let resposta = document.getElementsByName("questao9");
+    let valorResposta = "";
+
+    for (let contador9 = 0; contador9 < resposta.length; contador9++) {
+        if (resposta[contador9].checked) {
+            valorResposta = resposta[contador9].value;
+            if (valorResposta == 'certo') {
+                pg9.style.display = 'none';
+                resCerta.style.display = 'grid';
+                setTimeout(() => {
+                    resCerta.style.display = 'none';
+                    pg10.style.display = 'grid';
+                }, 2000);
+            }
+            else {
+                pg9.style.display = 'none';
+                resErrada.style.display = 'grid';
+                setTimeout(() => {
+                    resErrada.style.display = 'none';
+                    pg10.style.display = 'grid';
+                }, 2000);
+            }
+        }
+    }
+}
+
+document.getElementById("btnPergunta9").addEventListener("click", responder9);
+
