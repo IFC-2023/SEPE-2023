3fefa9bfd56146a859b8cd8df421fc5c28033de6
Joaovictor23t4 <joaovictor239090@gmail.com>
2023-10-05 19:09:41 -0300

    teste

---
+++ front/site/quiz/teste.php
@@ -0,0 +1,20 @@
+<?php
+echo "<tr>
+<td>Teste</td>
+<td>Teste</td>
+</tr>" 
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teste</title>
+</head>
+<body>
+    <table>
+
+    </table>
+</body>
+</html>
