ef258d29eeb6c929f5c56f6b23f55eab76171a93
Joaovictor23t4 <joaovictor239090@gmail.com>
2023-10-10 15:47:22 -0300

    mudancas

--- front/site/quiz/js/quiz.js
+++ front/site/quiz/js/quiz.js
@@ -22,15 +22,7 @@ function comecar() {
   pg1.style.display = "grid";
 }
 
-document.getElementById("btnComecar").addEventListener("click", () => {
-    const nome = document.getElementById("nomeUsuario").value;
-  if (nome !== "") {
-    comecar();
-  } else {
-    alert(`Digite um apelido no campo de apelido:`);
-  }
-});
-
+document.getElementById("btnComecar").addEventListener("click", comecar);
 function responder1() {
   let resposta = document.getElementsByName("questao1");
   let valorResposta = "";
