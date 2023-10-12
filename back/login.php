987f568ce7bc3a0c05a51428fc42d1ef97e1b95f
LuizBlume <luiz.fernando.251107@gmail.com>
2023-10-12 15:58:05 -0300

    LOGIN

--- back/login.php
+++ back/login.php
@@ -102,37 +102,6 @@ if (isset($_POST['submit-cad'])) {
         </p>
       </form>
     </div>
-
-    <!--FORMULÁRIO DE CADASTRO-->
-    <div id="cadastro">
-      <form method="post" action="login.php"> 
-        <h1>Cadastro</h1> 
-         
-        <p> 
-          <label for="nome_cad">Seu nome</label>
-          <input id="nome_cad" name="nome_cad" required="required" type="text" placeholder="nome"/>
-        </p>
-         
-        <p> 
-          <label for="email_cad">Seu e-mail</label>
-          <input id="email_cad" name="email_cad" required="required" type="email" placeholder="contato@gmail.com"/> 
-        </p>
-         
-        <p> 
-          <label for="senha_cad">Sua senha</label>
-          <input id="senha_cad" name="senha_cad" required="required" type="password" placeholder="ex. 1234"/>
-        </p>
-         
-        <p> 
-          <input type="submit" name="submit-cad" value="Cadastrar"/> 
-        </p>
-         
-        <p class="link">  
-          Já tem conta?
-          <a href="#paralogin"> Ir para Login </a>
-        </p>
-      </form>
-    </div>
   </div>
 </div>  
 
