6655bf4d7d1efe2e6ce065fe21f1a13216fa2ab0
Joaovictor23t4 <joaovictor239090@gmail.com>
2023-10-12 17:55:23 -0300

    correcao-login

--- back/protecao.php
+++ back/protecao.php
@@ -1,11 +1,11 @@
 <?php 
 
-if (!isset($_SESSION)) {
+if(!isset($_SESSION)) {
     session_start();
 }
 
-if (!isset($_SESSION['nome'])) {
-    die("Você não está logado, por favor faça o login ou cadastre-se no site. <p> <a href=\"../../../back/login.php\">Logar</a> </p>");
+if(!isset($_SESSION['nome'])) {
+    die("Erro: Você não está logado! <p><a href=\"../../../back/login.php\">Logar</p>");
 }
 
 ?>
