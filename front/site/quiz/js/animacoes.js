7880282cee1a7963c7ac3922e709caa09207af6e
Joaovictor23t4 <joaovictor239090@gmail.com>
2023-10-18 08:52:18 -0300

    adicionado-link-e-hover-sair

--- front/site/quiz/js/animacoes.js
+++ front/site/quiz/js/animacoes.js
@@ -9,6 +9,7 @@ let romana = document.getElementById("linkRomana");
 let mesopotamia = document.getElementById("link-dropdown-excessao");
 let mitologia = document.getElementById("link-mitologia");
 let quiz = document.getElementById("link-quiz");
+let sairLogin = document.getElementById("link-sair");
 
 function mouseSair() {
     let elementoHome = document.getElementById("link-home");
@@ -154,3 +155,13 @@ if (quiz !== null) {
 
     document.getElementById("link-quiz").addEventListener("mouseleave", linkQuizSair);
 }
+
+function sairLogout() {
+    let elementoHome = document.getElementById("link-sair");
+    elementoHome.classList.add("saiuLogin");
+    setTimeout(() => {
+        elementoHome.classList.remove("saiuLogin");
+    }, 300);
+}
+
+document.getElementById("link-sair").addEventListener("mouseleave", sairLogout);
